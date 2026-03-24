ea20b75b5e6c38d84d9530551af958fc0a8b3fba
vanhadson <vanhadson@gmail.com>
2026-03-24 20:04:16 +0300

    installed vite, mobx, mobx-react-observer

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import observerPlugin from "mobx-react-observer/babel-plugin";
+
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          observerPlugin(
+              // optional
+              { exclude: ["src/ui-components/**"] }
+          ),
+        ],
+      },
+    }),
+  ],
+});
