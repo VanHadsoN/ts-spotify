c26fbc7fd3bb884a661eb5c4f8c321068618371e
vanhadson <vanhadson@gmail.com>
2026-03-25 19:04:01 +0300

    feat(store): add "constructor()" to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,12 @@
+import { makeAutoObservable } from 'mobx';
+
 class MusicPlayerStore {
     isPlaying: boolean = false;
     currentTrack: string | null = null;
+
+    constructor() {
+        makeAutoObservable(this);
+    }
 
     play(track: string) {
         this.currentTrack = track;
