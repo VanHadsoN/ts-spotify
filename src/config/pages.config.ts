0d2c1f57bcbf320da3a2db68f95a1b5f7ba8852c
vanhadson <vanhadson@gmail.com>
2026-03-25 19:37:22 +0300

    feat(routing): centralize app routes in PagesConfig

---
+++ src/config/pages.config.ts
@@ -0,0 +1,21 @@
+export class PagesConfig {
+    static HOME = '/';
+    static DISCOVER = '/discover';
+    static RADIO = '/radio';
+
+    static MADE_FOR_YOU = '/made-for-you';
+    static RECENTLY_PLAYED = '/recently-played';
+    static LIKED_SONGS = '/liked-songs';
+
+    static PLAYLIST(id: string) {
+        return `/playlist/${id}`;
+    }
+
+    static ALBUMS(id?: string) {
+        return '/albums' + (id ? `/${id}` : '');
+    }
+
+    static ARTISTS(id?: string) {
+        return '/artists' + (id ? `/${id}` : '');
+    }
+}
