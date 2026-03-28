e4ad93766cfbd77dbfff73ddb4946b46b94c246c
vanhadson <vanhadson@gmail.com>
2026-03-28 11:38:56 +0300

    build: configure Vite alias, Tailwind plugin, and MobX Babel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import observerPlugin from "mobx-react-observer/babel-plugin";
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react({
       babel: {
         plugins: [
@@ -15,4 +18,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 });
