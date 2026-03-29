c731d8713fafa0c31e68d74e00b0cf815e4b5bdd
vanhadson <vanhadson@gmail.com>
2026-03-29 11:22:27 +0300

    refactor(layout): replace Sidebar with LeftSidebar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,12 @@
 import type { PropsWithChildren } from "react";
+import {RightSidebar} from "@/components/layout/right-sidebar/RightSidebar.tsx";
+import {LeftSidebar} from "@/components/layout/left-sidebar/LeftSidebar.tsx";
 
 export default function Layout ({ children }: PropsWithChildren<unknown>) {
 
     return (
         <div className='h-full grid grid-cols-[1fr_3.5fr_1.2fr]'>
-            <Sidebar />
+            <LeftSidebar />
             <main className='py-6 px-4'>
                 {children}
             </main>
