6a21ca66e96a36ded0e28abb097f49850bc6f381
vanhadson <vanhadson@gmail.com>
2026-03-29 11:46:10 +0300

    refactor(layout): replace right sidebar menu with themed placeholder

--- src/components/layout/right-sidebar/RightSidebar.tsx
+++ src/components/layout/right-sidebar/RightSidebar.tsx
@@ -1,10 +1,3 @@
-import {LIBRARY_MENU_ITEMS, MENU_ITEMS} from "@/data/menu.data.ts";
-import {Menu} from "@/components/layout/left-sidebar/Menu.tsx";
-
 export function RightSidebar() {
-    return <aside>
-
-        <Menu items={MENU_ITEMS} />
-
-    </aside>
+    return <div className="bg-bg-secondary">RightSidebar</div>
 }
