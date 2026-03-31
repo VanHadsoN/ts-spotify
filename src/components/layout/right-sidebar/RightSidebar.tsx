4ffceb7321674031d80071cd72eac740fab25d02
vanhadson <vanhadson@gmail.com>
2026-03-31 21:29:21 +0300

    chore(right-sidebar): add temporary lyrics mockup

--- src/components/layout/right-sidebar/RightSidebar.tsx
+++ src/components/layout/right-sidebar/RightSidebar.tsx
@@ -4,7 +4,37 @@
 export function RightSidebar() {
     return <div className="bg-bg-secondary p-layout">
 
-        RightSidebar
+        <div className='bg-bg-secondary p-layout'>
+            <div className='text-primary font-medium mb-5'>[ Verse 1 ]</div>
+            <p>Well, on a train, I met a dame</p>
+            <p>She rather handsome, we kinda looked the same</p>
+            <p>She was pretty, from New York City</p>
+            <p>I'm walking down that old fair lane</p>
+            <p>I'm in heat, I'm in love</p>
+            <p>But I just couldn't tell her so</p>
+
+            <div className='text-primary font-medium mb-5'>[ Chorus ]</div>
+            <p>I said, train kept a-rolling all night long</p>
+            Train kept a-rolling all night long
+            Train kept a-rolling all night long
+            Train kept a-rolling all night long
+            With a heave, and a ho
+            But I just couldn't tell her so, no, no, no
+
+            <div className='text-primary font-medium mb-5'>[ Bridge ]</div>
+            <p>Well, get along, sweet little woman, get along</p>
+            On your way, get along, sweet little woman, get along
+            On your way, I'm in heat, I'm in love
+            But I just couldn't tell her so, no, no, no
+
+            <div className='text-primary font-medium mb-5'>[ Verse 2 ]</div>
+            <p>Yeah, train, I met a dame</p>
+            She rather handsome, we kinda looked the same
+            She was pretty, from New York City
+            I'm walking down that old fair lane
+            I'm in heat, I'm in love
+            But I just couldn't tell her so
+        </div>
 
     </div>
 }
