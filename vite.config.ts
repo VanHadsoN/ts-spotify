fddfd73456b0752e29f7bf7ce6f16e20360a238e
vanhadson <vanhadson@gmail.com>
2026-04-07 21:07:19 +0300

    fix(vite): remove mobx-react-observer babel plugin config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import react from '@vitejs/plugin-react';
-import observerPlugin from "mobx-react-observer/babel-plugin";
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
@@ -7,16 +6,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react({
-      babel: {
-        plugins: [
-          observerPlugin(
-              // optional
-              { exclude: ["src/ui-components/**"] }
-          ),
-        ],
-      },
-    }),
+    react(),
   ],
   resolve: {
     alias: {
