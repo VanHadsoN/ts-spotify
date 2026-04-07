50347481fff9a00a653462f97764a13649a2d23b
vanhadson <vanhadson@gmail.com>
2026-04-07 21:21:04 +0300

    feat(app): add artist header block under banner

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ function App() {
   return (
       <div>
           <SearchField />
-        <img src="/banner.png" alt="" />
+          <div>
+              <img src="/banner.png" alt="" className="rounded-xl" />
+
+              <h1 className='text-2xl font-bold mb-1'>Aerosmith</h1>
+              <h2 className='text-primary font-semibold'>25.6m Listeners</h2>
+          </div>
+
       </div>
   )
 }
