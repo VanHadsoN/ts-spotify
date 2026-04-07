b68ae33e9420ab8e3aac71e8f8ecc422fda12815
vanhadson <vanhadson@gmail.com>
2026-04-07 21:20:34 +0300

    style(layout): adjust main padding

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,7 +7,7 @@ export default function Layout ({ children }: PropsWithChildren<unknown>) {
     return (
         <div className='min-h-screen h-full grid grid-cols-[1fr_4fr_1.1fr]'>
             <LeftSidebar />
-            <main className='p-layout'>
+            <main className='p-12 py-layout'>
                 {children}
             </main>
             <RightSidebar />
