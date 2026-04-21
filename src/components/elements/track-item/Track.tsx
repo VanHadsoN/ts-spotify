ac1ea690b4da4908686cc7d8ec8d61b10bcb4fe3
vanhadson <vanhadson@gmail.com>
2026-04-21 21:31:30 +0300

    refactor(track): replace inline dayjs formatting with transformDuration

--- src/components/elements/track-item/Track.tsx
+++ src/components/elements/track-item/Track.tsx
@@ -1,21 +1,20 @@
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
 
 import type { ITrack } from "@/types/track.types";
 import {Ellipsis, Heart} from "lucide-react";
 import {TrackInfo} from "@/components/ui/track-info/TrackInfo";
+import {transformDuration} from "@/utils/transform-duration.ts";
 
 interface Props {
     track: ITrack;
 };
 
-dayjs.extend(utc);
+
 export function Track({track}: Props) {
     return (
         <div className="border-b border-player-bg/50 py-7 last:border-0">
             <TrackInfo
                 title={track.name}
-                subTitle={dayjs.unix(track.duration).utc().format('m:ss')}
+                subTitle={transformDuration(track.duration)}
                 image={undefined}
             />
                 <div className="flex shrink-0 items-center gap-4">
