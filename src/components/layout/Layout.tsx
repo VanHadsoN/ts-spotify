09c24f98b675535d63451ffc79279bb734708554
vanhadson <vanhadson@gmail.com>
2026-04-21 21:33:25 +0300

    feat(layout): render AudioPlayer as persistent bottom bar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,17 +1,21 @@
 import type { PropsWithChildren } from "react";
 import {RightSidebar} from "@/components/layout/right-sidebar/RightSidebar.tsx";
 import {LeftSidebar} from "@/components/layout/left-sidebar/LeftSidebar.tsx";
+import {AudioPlayer} from "@/components/elements/player/AudioPlayer.tsx";
 
 export default function Layout ({ children }: PropsWithChildren<unknown>) {
 
     return (
-        <div className='min-h-screen h-full grid grid-cols-[1fr_4fr_1.1fr]'>
-            <LeftSidebar />
-            <main className='p-12 py-layout'>
-                {children}
-            </main>
-            <RightSidebar />
-        </div>
-        )
+        <>
+            <div className='min-h-screen h-full grid grid-cols-[1fr_4fr_1.1fr]'>
+                <LeftSidebar />
+                <main className='p-12 py-layout'>
+                    {children}
+                </main>
+                <RightSidebar />
+            </div>
+            <AudioPlayer />
+        </>
+    )
 
 }
