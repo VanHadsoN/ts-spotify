525a1a73c62ae1fba64c4082b582ce9611f08512
vanhadson <vanhadson@gmail.com>
2026-04-21 21:33:52 +0300

    feat(audio-player): add duration display and volume icon states

--- src/components/elements/player/AudioPlayer.tsx
+++ src/components/elements/player/AudioPlayer.tsx
@@ -1,13 +1,16 @@
 import {TrackInfo} from "@/components/ui/track-info/TrackInfo";
 import {musicPlayerStore} from "@/store/store";
-import {Pause, Play, SkipBack, SkipForward} from "lucide-react";
+import {Pause, Play, SkipBack, SkipForward, Volume, Volume1, Volume2} from "lucide-react";
+import {transformDuration} from "@/utils/transform-duration.ts";
 
 interface Props {}
 
 export function AudioPlayer({}: Props) {
     if (!musicPlayerStore.currentTrack) return null;
 
-    return <div>
+    return <div className="w-full py-8 px-10 bg-player-bg
+            border-t border-white/10
+            flex items-center justify-between fixed bottom-0 left-0">
         <TrackInfo
             title={musicPlayerStore.currentTrack.name}
             subTitle={musicPlayerStore.currentTrack.artist.name}
@@ -20,16 +23,33 @@ export function AudioPlayer({}: Props) {
                     <SkipBack />
                 </button>
 
-                <button className="rounded-full
-                bg-gradient-to-r from-[#3C3D41] to-[#1F2026]
-                p-5 border border-player-bg bodredr-solid
-                duration-300 hover:shadow">
+                <button className="rounded-full bg-gradient-to-r from-[#3C3D41] to-[#444549]
+                p-4 border border-player-bg border-solid duration-300 hover:shadow">
                     {musicPlayerStore.isPlaying ? <Pause /> : <Play />}
                 </button>
 
                 <button>
                     <SkipForward />
                 </button>
+            </div>
+
+            <div>
+                <span>0:00</span>
+                <input type="range"/>
+                <span>
+                    {transformDuration(musicPlayerStore.currentTrack.duration)}
+                </span>
+            </div>
+
+            <div>
+                {
+                    musicPlayerStore.volume === 0 ? (<Volume />) :
+                    musicPlayerStore.volume < 50 ? (<Volume1 />) :
+                    (<Volume2 />
+                    )
+                }
+
+                {/* TODO: ProgressBar */}
             </div>
         </div>
     </div>
