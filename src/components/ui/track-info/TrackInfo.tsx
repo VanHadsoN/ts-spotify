482268a248b82ef4acd41bc3c7a4eac112897234
vanhadson <vanhadson@gmail.com>
2026-05-25 21:15:55 +0300

    feat(track-info): toggle playback on cover click

--- src/components/ui/track-info/TrackInfo.tsx
+++ src/components/ui/track-info/TrackInfo.tsx
@@ -1,22 +1,26 @@
+import {musicPlayerStore} from "@/store/store.ts";
+import type {ITrack} from "@/types/track.types.ts";
+
 interface Props {
     image: string;
     title: string;
     subTitle: string;
+    track?: ITrack;
 }
 
-export function TrackInfo({ title, subTitle, image }: Props) {
+export function TrackInfo({ title, subTitle, image, track }: Props) {
     return (
         <div className="flex items-center gap-3">
             {/* TODO: Circle progress-bar */}
             {/* TODO: Play/pause button when hover title or cover */}
 
-            <div>
+            <button onClick={() => musicPlayerStore.togglePlayPause()}>
                 <img
                     src={image}
                     alt={title}
                     className="w-12 h-12 rounded-full"
                 />
-            </div>
+            </button>
 
             <div className="min-w-0 flex-1">
                 <div className="text-lg text-white font-medium">{title}</div>
