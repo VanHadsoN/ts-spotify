e7ee5164fd8525d56ef8d4e53a990603db0a80a5
vanhadson <vanhadson@gmail.com>
2026-05-26 20:40:34 +0300

    feat(track-info): add circular progress for active track

--- src/components/ui/track-info/TrackInfo.tsx
+++ src/components/ui/track-info/TrackInfo.tsx
@@ -1,5 +1,8 @@
 import {musicPlayerStore} from "@/store/store.ts";
 import type {ITrack} from "@/types/track.types.ts";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import {observer} from "mobx-react-lite";
 
 interface Props {
     image: string;
@@ -8,32 +11,38 @@ interface Props {
     track?: ITrack;
 }
 
-export function TrackInfo({ title, subTitle, image, track }: Props) {
-    return (
-        <div className="flex items-center gap-3">
-            {/* TODO: Circle progress-bar */}
-            {/* TODO: Play/pause button when hover title or cover */}
+export const TrackInfo = observer(
+    function TrackInfo({ title, subTitle, image, track }: Props) {
 
-            {track ? (
-                <button onClick={() => musicPlayerStore.setTrack(track)}>
+        const isActive = musicPlayerStore.currentTrack?.name === track?.name;
+
+        return (
+            <div className="flex items-center gap-3">
+                {/* TODO: Circle progress-bar */}
+                {/* TODO: Play/pause button when hover title or cover */}
+
+                {track ? (
+                    <button onClick={() => musicPlayerStore.setTrack(track)}>
+                        {isActive && <CircularProgressbar value={musicPlayerStore.progress}/>}
+
+                        <img
+                            src={image}
+                            alt={title}
+                            className="w-12 h-12 rounded-full"
+                        />
+                    </button>
+                ) : (
                     <img
                         src={image}
                         alt={title}
                         className="w-12 h-12 rounded-full"
                     />
-                </button>
-            ) : (
-                <img
-                    src={image}
-                    alt={title}
-                    className="w-12 h-12 rounded-full"
-                />
-            )}
+                )}
 
-            <div>
-                <div className="text-lg text-white font-medium">{title}</div>
-                <div>{subTitle}</div>
+                <div>
+                    <div className="text-lg text-white font-medium">{title}</div>
+                    <div>{subTitle}</div>
+                </div>
             </div>
-        </div>
-    )
-}
+        );
+    });
