903e321c6b7d22c4300f123d24bd87be9724f6c1
vanhadson <vanhadson@gmail.com>
2026-06-04 17:38:33 +0300

    refactor(search-field): make SearchField a controlled component

--- src/components/elements/search-field/SearchField.tsx
+++ src/components/elements/search-field/SearchField.tsx
@@ -1,10 +1,22 @@
 import {Search} from "lucide-react";
+import {type ChangeEvent } from "react";
 
-export function SearchField() {
+interface Props {
+    value: string;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export function SearchField({value, onChange}: Props) {
     return <div>
         <label className="flex items-center gap-3 group">
             <Search className="opacity-40 group-focus-within:opacity-100 duration-300" />
-            <input type="search" placeholder="Search for songs, artists, etc..." className="bg-transparent w-full" />
+            <input
+                type="search"
+                placeholder="Search for songs, artists, etc..."
+                className="bg-transparent w-full"
+                value={value}
+                onChange={onChange}
+            />
         </label>
 
     </div>
