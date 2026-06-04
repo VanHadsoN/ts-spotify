6c1a9c3c1dd74002503d877cf2aebc527a6cd019
vanhadson <vanhadson@gmail.com>
2026-06-04 17:28:55 +0300

    chore: wrap app root with NuqsAdapter

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Layout from './components/layout/Layout.tsx'
+import {NuqsAdapter} from "nuqs/adapters/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <Layout>
-          <App />
-      </Layout>
+      <NuqsAdapter>
+          <Layout>
+              <App />
+          </Layout>
+      </NuqsAdapter>
   </StrictMode>,
 )
