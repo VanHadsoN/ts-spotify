b1064a37261a829ec4af37adeaa13b7c298509ad
vanhadson <vanhadson@gmail.com>
2026-06-06 21:18:04 +0300

    feat(track): add favorite toggle with MobX store

--- src/components/elements/track-item/Track.tsx
+++ src/components/elements/track-item/Track.tsx
@@ -3,13 +3,15 @@ import type { ITrack } from "@/types/track.types";
 import {Ellipsis, Heart} from "lucide-react";
 import {TrackInfo} from "@/components/ui/track-info/TrackInfo";
 import {transformDuration} from "@/utils/transform-duration.ts";
+import {favoriteStore} from "@/store/favorite.store.ts";
+import { observer } from "mobx-react-observer";
 
 interface Props {
     track: ITrack;
 };
 
 
-export function Track({track}: Props) {
+export const Track = observer(function Track({track}: Props) {
     return (
         <div className="flex items-center justify-between gap-4 border-b border-player-bg/50 py-7 last:border-0">
             <div className="min-w-0 flex-1">
@@ -21,13 +23,15 @@ export function Track({track}: Props) {
                 />
             </div>
             <div className="flex shrink-0 items-center gap-4">
-                <button type="button" aria-label="В избранное">
-                    <Heart className="text-primary hover:fill-primary" />
+                <button onClick={() => {favoriteStore.toggleFavorite(track.name)}} >
+                    <Heart className="text-primary opacity-85 duration-300 hover:opacity-100"
+                           fill={favoriteStore.favoritesName.includes(track.name) ? 'var(--color-primary)' : 'none'}
+                    />
                 </button>
-                <button type="button" aria-label="Ещё">
+                <button>
                     <Ellipsis className="opacity-65 hover:opacity-100" />
                 </button>
             </div>
         </div>
-    )
-}
+    );
+})
