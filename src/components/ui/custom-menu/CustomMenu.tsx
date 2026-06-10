3a6114fef29a8c0443dc2d91d731734e5a3cd6a6
vanhadson <vanhadson@gmail.com>
2026-06-10 12:27:23 +0300

    style(custom-menu): change background color for CustomMenu

--- src/components/ui/custom-menu/CustomMenu.tsx
+++ src/components/ui/custom-menu/CustomMenu.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 export function CustomMenu({ children }: Props) {
-    return <div className="w-48 fade-in bg-zinc-700/30 p-4 rounded-md absolute left-0 z-10"
+    return <div className="w-48 fade-in bg-[#2B2B30] p-4 rounded-md absolute left-0 z-10"
                 style={{ top: 'calc(100% + 0.5rem)' }}
     >
         {children}
