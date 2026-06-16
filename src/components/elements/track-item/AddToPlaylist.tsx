1e5d8512ff9fcb10fe2ad098aeba131e5e805003
vanhadson <vanhadson@gmail.com>
2026-06-16 11:28:46 +0300

    feat(track): implement add-to-playlist dropdown menu

--- src/components/elements/track-item/AddToPlaylist.tsx
+++ src/components/elements/track-item/AddToPlaylist.tsx
@@ -1,12 +1,10 @@
 import type { ITrack } from "@/types/track.types";
-import {Ellipsis, Heart, Menu, Plus} from "lucide-react";
-import {TrackInfo} from "@/components/ui/track-info/TrackInfo";
-import {transformDuration} from "@/utils/transform-duration.ts";
-import {favoriteStore} from "@/store/favorite.store.ts";
+import { Ellipsis } from "lucide-react";
 import { observer } from "mobx-react-observer";
 import {useState} from "react";
 import {CustomMenu} from "@/components/ui/custom-menu/CustomMenu.tsx";
 import {playlistStore} from "@/store/playlist.store.ts";
+import cn from "clsx";
 
 interface Props {
     track: ITrack;
@@ -14,7 +12,7 @@ interface Props {
 
 
 export const AddToPlaylist = observer(function AddToPlaylist({track}: Props) {
-    const [value, setValue] = useState("");
+    // const [value, setValue] = useState("");
     const [isShow, setIsShow] = useState(false);
     return (
         <div className="relative">
@@ -33,8 +31,20 @@ export const AddToPlaylist = observer(function AddToPlaylist({track}: Props) {
                             <button
                                 key={playlist.name}
                                 className="w-full text-left px-4 py-2 hover:bg-white/10"
-
-                            ></button>
+                                onClick={() => {
+                                    playlistStore.toggleTrackInPlaylist(
+                                        playlist.name,
+                                        track.name
+                                    )
+                                }}
+                            >
+                                <span
+                                    className={cn('transition-opacity duration-300', isAdded ?
+                                        'opacity-100' : 'opacity-60')}
+                                >
+                                    {playlist.name}
+                                </span>
+                            </button>
                         )
                     })}
                 </CustomMenu>
