003f6d856d7cbedc816e9a1865617d94bd93b72e
vanhadson <vanhadson@gmail.com>
2026-06-17 21:15:14 +0300

    feat(lyrics): render synced lyrics from data

--- src/components/layout/right-sidebar/Lyrics.tsx
+++ src/components/layout/right-sidebar/Lyrics.tsx
@@ -1,5 +1,3 @@
-// TODO: add dynamic lyrics
-
 import { Play } from 'lucide-react'
 import styles from './Lyrics.module.css'
 import {LYRICS} from "@/data/lyrics.data.ts";
@@ -15,12 +13,11 @@ export function Lyrics() {
         <div className={styles.lyrics}>
 
             {lyric?.lines?.map((line, index) => (
-                <Fragment
-                    key={index}
-                >
+                <Fragment key={index}>
+                    {line.section && <br />}
                     {line.section && <div>[ {line.section} ]</div>}
 
-                    <p className={musicPlayerStore.currentTime >= line.time ? styles.active : undefined}>
+                    <button className={musicPlayerStore.currentTime >= line.time ? styles.active : undefined}>
                         {musicPlayerStore.currentTime >= line.time && (
                             <Play
                                 fill="var(--color-primary)"
@@ -29,47 +26,10 @@ export function Lyrics() {
                             />
                         )}
                         {line.text}
-                    </p>
+                    </button>
 
-                    {line.section && <br />}
                 </Fragment>
             ))}
-
-            <div>[ Verse 1 ]</div>
-            <p>Well, on a train, I met a dame</p>
-            <p>She rather handsome, we kinda looked the same</p>
-            <p>She was pretty, from New York City</p>
-            <p>I'm walking down that old fair lane</p>
-            <p className={styles.active}>
-                <Play
-                    fill="var(--color-primary)"
-                    className={styles.icon}
-                    size={10}
-                />
-                I'm in heat, I'm in love</p>
-            <p>But I just couldn't tell her so</p>
-            <br />
-            <div>[ Chorus ]</div>
-            <p>I said, train kept a-rolling all night long</p>
-            <p>Train kept a-rolling all night long</p>
-            <p>Train kept a-rolling all night long</p>
-            <p>Train kept a-rolling all night long</p>
-            <p>With a heave, and a ho</p>
-            <p>But I just couldn't tell her so, no, no, no</p>
-            <br />
-            <div>[ Bridge ]</div>
-            <p>Well, get along, sweet little woman, get along</p>
-            <p>On your way, get along, sweet little woman, get along</p>
-            <p>On your way, I'm in heat, I'm in love</p>
-            <p>But I just couldn't tell her so, no, no, no</p>
-            <br />
-            <div>[ Verse 2 ]</div>
-            <p>Yeah, train, I met a dame</p>
-            <p>She rather handsome, we kinda looked the same</p>
-            <p>She was pretty, from New York City</p>
-            <p>I'm walking down that old fair lane</p>
-            <p>I'm in heat, I'm in love</p>
-            <p>But I just couldn't tell her so</p>
         </div>
     </div>
 }
