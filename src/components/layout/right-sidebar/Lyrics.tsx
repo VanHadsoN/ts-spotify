4f2fd5fbb91bd28d454ceeed5962178d2cc021a7
vanhadson <vanhadson@gmail.com>
2026-06-19 21:27:38 +0300

    fix(lyrics): show indicator only for active line

--- src/components/layout/right-sidebar/Lyrics.tsx
+++ src/components/layout/right-sidebar/Lyrics.tsx
@@ -3,10 +3,19 @@ import styles from './Lyrics.module.css'
 import {LYRICS} from "@/data/lyrics.data.ts";
 import {musicPlayerStore} from "@/store/store.ts";
 import {Fragment} from "react";
+import {observer} from "mobx-react-lite";
 
-export function Lyrics() {
+function LyricsInner() {
     const lyric = LYRICS.find(
         lyric => lyric.trackName === musicPlayerStore.currentTrack?.name
+    );
+
+    if (!lyric) {
+        return <div className={styles.lyrics}>Lyrics not found</div>
+    }
+
+    const activeLineIndex = lyric.lines.findLastIndex(
+        line => musicPlayerStore.currentTime >= line.time
     );
 
     return <div>
@@ -17,19 +26,24 @@ export function Lyrics() {
                     {line.section && <br />}
                     {line.section && <div>[ {line.section} ]</div>}
 
-                    <button className={musicPlayerStore.currentTime >= line.time ? styles.active : undefined}>
-                        {musicPlayerStore.currentTime >= line.time && (
-                            <Play
-                                fill="var(--color-primary)"
-                                className={styles.icon}
-                                size={10}
-                            />
-                        )}
-                        {line.text}
+                    <button className={index === activeLineIndex ? styles.active : undefined}
+                            onClick={() => {musicPlayerStore.seek(line.time)}}
+                    >
+                        <p>
+                            {index === activeLineIndex && (
+                                <Play
+                                    fill="var(--color-primary)"
+                                    className={styles.icon}
+                                    size={10}
+                                />
+                            )}
+                            {line.text}
+                        </p>
                     </button>
-
                 </Fragment>
             ))}
         </div>
     </div>
 }
+
+export const Lyrics = observer(LyricsInner);
