0e7c26091230290fef8370c7cc602c284cbeaf9c
vanhadson <vanhadson@gmail.com>
2026-07-14 18:37:26 +0300

    feat(sidebar): use NavLink in Menu and highlight active route

--- src/components/layout/left-sidebar/Menu.tsx
+++ src/components/layout/left-sidebar/Menu.tsx
@@ -1,5 +1,7 @@
+import type { ReactNode } from "react";
+import { NavLink } from "react-router-dom";
+
 import type { IMenuItem } from "@/types/menu.types.ts";
-import type {ReactNode} from "react";
 
 interface Props {
     items: IMenuItem[];
@@ -8,25 +10,38 @@ interface Props {
 }
 
 export function Menu({ items, title, children }: Props) {
-    return <div>
-        {title && <div className="opacity-60 text-xxs uppercase font-medium mb-5">{title}</div>}
+    return (
+        <div>
+            {title && <div className="opacity-60 text-xxs uppercase font-medium mb-5">{title}</div>}
 
-        {items.length === 0 && <div>No items found</div>}
+            {items.length === 0 && <div>No items found</div>}
 
-        <ul>
-            {items.map((item) => (
-                <li key={item.name}>
-                    <a className="flex gap-3 items-center mb-5 group"
-                       href="#"
-                    >
-                        {item.icon && <item.icon className="group-hover:text-primary" />}
-                        <span className="group-hover:text-primary duration-300 font-medium">{item.name}</span>
-                    </a>
-                </li>
-            ))}
-        </ul>
+            <ul>
+                {items.map((item) => (
+                    <li key={item.link}>
+                        <NavLink to={item.link} className="flex gap-3 items-center mb-5 group">
+                            {({ isActive }) => (
+                                <>
+                                    {item.icon && (
+                                        <item.icon className={isActive ? "text-primary" : "group-hover:text-primary"} />
+                                    )}
+                                    <span
+                                        className={
+                                            isActive
+                                                ? "text-primary duration-300 font-medium"
+                                                : "group-hover:text-primary duration-300 font-medium"
+                                        }
+                                    >
+                    {item.name}
+                  </span>
+                                </>
+                            )}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
 
-        {children}
-
-    </div>
+            {children}
+        </div>
+    );
 }
