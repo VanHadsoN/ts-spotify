03a5f1e56b939215610fe98c156fc9d1eba34de5
vanhadson <vanhadson@gmail.com>
2026-07-24 18:01:58 +0300

    feat: add album details to track data

--- src/data/tracks.data.ts
+++ src/data/tracks.data.ts
@@ -7,6 +7,7 @@ type RawTrack = Omit<ITrack, "id">;
 export const RAW_TRACKS: RawTrack[] = [
     {
         name: "Train Kept A Rollin",
+        album: "Get Your Wings",
         file: "/audio/Aerosmith - Train Kept A Rollin'.mp3",
         artist: ARTISTS[0],
         cover: "/cover/Train_Kept_A_Rollin.jpg",
@@ -14,6 +15,7 @@ export const RAW_TRACKS: RawTrack[] = [
     },
     {
         name: "Green River",
+        album: "Green River",
         file: "/audio/Creedence Clearwater Revival - Green River.mp3",
         artist: ARTISTS[1],
         cover: "/cover/Green_River.jpg",
@@ -21,6 +23,7 @@ export const RAW_TRACKS: RawTrack[] = [
     },
     {
         name: "Zitti e buoni",
+        album: "Teatro d’ira: Vol. I.",
         file: "/audio/Maneskin - Zitti e buoni.mp3",
         artist: ARTISTS[2],
         cover: "/cover/Zitti_e_buoni.jpg",
@@ -28,6 +31,7 @@ export const RAW_TRACKS: RawTrack[] = [
     },
     {
         name: "Little By Little",
+        album: "Heathen Chemistry",
         file: "/audio/Oasis - Little By Little.mp3",
         artist: ARTISTS[3],
         cover: "/cover/Little_By_Little.jpg",
@@ -35,6 +39,7 @@ export const RAW_TRACKS: RawTrack[] = [
     },
     {
         name: "Last of the Mohikans Theme",
+        album: "The Last of the Mohicans (Original Motion Picture Soundtrack)",
         file: "/audio/Trevor Jones - Last of the Mohikans Theme.mp3",
         artist: ARTISTS[4],
         cover: "/cover/Last_of_the_Mohikans_Theme.jpg",
