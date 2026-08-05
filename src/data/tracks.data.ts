1c44da39f9967b061fe28abeb58e1f9a0ae3ce6d
vanhadson <vanhadson@gmail.com>
2026-08-05 14:10:40 +0300

    chore(assets): standardize audio and cover filenames

--- src/data/tracks.data.ts
+++ src/data/tracks.data.ts
@@ -8,41 +8,41 @@ export const RAW_TRACKS: RawTrack[] = [
     {
         name: "Train Kept A Rollin",
         album: "Get Your Wings",
-        file: "/audio/Aerosmith - Train Kept A Rollin'.mp3",
+        file: "/audio/aerosmith-train-kept-a-rollin.mp3",
         artist: ARTISTS[0],
-        cover: "/cover/Train_Kept_A_Rollin.jpg",
+        cover: "/cover/train-kept-a-rollin.jpg",
         duration: 335, // сек
     },
     {
         name: "Green River",
         album: "Green River",
-        file: "/audio/Creedence Clearwater Revival - Green River.mp3",
+        file: "/audio/creedence-clearwater-revival-green-river.mp3",
         artist: ARTISTS[1],
-        cover: "/cover/Green_River.jpg",
+        cover: "/cover/green-river.jpg",
         duration: 136, // сек
     },
     {
         name: "Zitti e buoni",
         album: "Teatro d’ira: Vol. I.",
-        file: "/audio/Maneskin - Zitti e buoni.mp3",
+        file: "/audio/maneskin-zitti-e-buoni.mp3",
         artist: ARTISTS[2],
-        cover: "/cover/Zitti_e_buoni.jpg",
+        cover: "/cover/zitti-e-buoni.jpg",
         duration: 194, // сек
     },
     {
         name: "Little By Little",
         album: "Heathen Chemistry",
-        file: "/audio/Oasis - Little By Little.mp3",
+        file: "/audio/oasis-little-by-little.mp3",
         artist: ARTISTS[3],
-        cover: "/cover/Little_By_Little.jpg",
+        cover: "/cover/little-by-little.jpg",
         duration: 294, // сек
     },
     {
         name: "Last of the Mohikans Theme",
         album: "The Last of the Mohicans (Original Motion Picture Soundtrack)",
-        file: "/audio/Trevor Jones - Last of the Mohikans Theme.mp3",
+        file: "/audio/trevor-jones-last-of-the-mohikans-theme.mp3",
         artist: ARTISTS[4],
-        cover: "/cover/Last_of_the_Mohikans_Theme.jpg",
+        cover: "/cover/last-of-the-mohikans-theme.jpg",
         duration: 169, // сек
     },
 ];
