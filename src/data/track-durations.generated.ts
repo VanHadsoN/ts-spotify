2029084394c794305bd563b3ba21d9c2ec370e3d
vanhadson <vanhadson@gmail.com>
2026-08-07 18:35:08 +0300

    refactor(tracks): use generated track durations

---
+++ src/data/track-durations.generated.ts
@@ -0,0 +1,8 @@
+// Этот файл создан автоматически.
+        // Не редактируйте его вручную.
+
+        export const TRACK_DURATIONS = Object.freeze({"/audio/aerosmith-train-kept-a-rollin.mp3": 335,
+"/audio/creedence-clearwater-revival-green-river.mp3": 136,
+"/audio/maneskin-zitti-e-buoni.mp3": 194,
+"/audio/oasis-little-by-little.mp3": 293,
+"/audio/trevor-jones-last-of-the-mohikans-theme.mp3": 168,});
--- src/data/tracks.data.ts
+++ src/data/tracks.data.ts
@@ -1,8 +1,9 @@
 import type { ITrack } from "../types/track.types.ts";
 import { ARTISTS } from "./artist.data.ts";
 import {generateTrackId} from "@/utils/track-id.ts";
+import {TRACK_DURATIONS} from "@/data/track-durations.generated.ts";
 
-type RawTrack = Omit<ITrack, "id">;
+type RawTrack = Omit<ITrack, "id" | "duration">;
 
 export const RAW_TRACKS: RawTrack[] = [
     {
@@ -11,7 +12,6 @@ export const RAW_TRACKS: RawTrack[] = [
         file: "/audio/aerosmith-train-kept-a-rollin.mp3",
         artist: ARTISTS[0],
         cover: "/cover/train-kept-a-rollin.jpg",
-        duration: 335, // сек
     },
     {
         name: "Green River",
@@ -19,7 +19,6 @@ export const RAW_TRACKS: RawTrack[] = [
         file: "/audio/creedence-clearwater-revival-green-river.mp3",
         artist: ARTISTS[1],
         cover: "/cover/green-river.jpg",
-        duration: 136, // сек
     },
     {
         name: "Zitti e buoni",
@@ -27,7 +26,6 @@ export const RAW_TRACKS: RawTrack[] = [
         file: "/audio/maneskin-zitti-e-buoni.mp3",
         artist: ARTISTS[2],
         cover: "/cover/zitti-e-buoni.jpg",
-        duration: 194, // сек
     },
     {
         name: "Little By Little",
@@ -35,7 +33,6 @@ export const RAW_TRACKS: RawTrack[] = [
         file: "/audio/oasis-little-by-little.mp3",
         artist: ARTISTS[3],
         cover: "/cover/little-by-little.jpg",
-        duration: 294, // сек
     },
     {
         name: "Last of the Mohikans Theme",
@@ -43,11 +40,13 @@ export const RAW_TRACKS: RawTrack[] = [
         file: "/audio/trevor-jones-last-of-the-mohikans-theme.mp3",
         artist: ARTISTS[4],
         cover: "/cover/last-of-the-mohikans-theme.jpg",
-        duration: 169, // сек
     },
 ];
+
+const durations: Readonly<Record<string, number>> = TRACK_DURATIONS;
 
 export const TRACKS: ITrack[] = RAW_TRACKS.map((track) => ({
     ...track,
     id: generateTrackId(track.file, track.artist.name),
+    duration: durations[track.file],
 }));
