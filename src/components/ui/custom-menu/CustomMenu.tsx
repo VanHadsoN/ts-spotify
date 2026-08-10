a120c62d687cfc1fe8ee3a021a4756a4308dbeb9
vanhadson <vanhadson@gmail.com>
2026-08-10 18:29:54 +0300

    refactor(custom-menu): support native div props and custom styles

--- src/components/ui/custom-menu/CustomMenu.tsx
+++ src/components/ui/custom-menu/CustomMenu.tsx
@@ -1,17 +1,21 @@
-import type { ReactNode } from 'react';
+import type { ComponentPropsWithoutRef } from 'react';
 import cn from "clsx";
 
-interface Props {
-    children: ReactNode;
+interface Props extends ComponentPropsWithoutRef<"div"> {
     side?: "left" | "right";
 }
 
-export function CustomMenu({ children, side }: Props) {
-    return <div className={cn("w-48 fade-in bg-[#2B2B30] p-1 rounded-md absolute z-10",
-        side === "left" ? "left-0" : side === "right" ? "right-0" : "left-1/2 -translate-x-1/2",
-        )}
-                style={{ top: "calc(100% + 0.5rem)" }}
-    >
-        {children}
-    </div>
+export function CustomMenu({ children, side, className, ...props }: Props) {
+    return (
+        <div
+            {...props}
+            className={cn("w-48 fade-in bg-[#2B2B30] p-1 rounded-md absolute z-10",
+                        side === "left" ? "left-0" : side === "right" ? "right-0" : "left-1/2 -translate-x-1/2",
+                className,
+            )}
+            style={{ top: "calc(100% + 0.5rem)", ...props.style }}
+        >
+            {children}
+        </div>
+    );
 }
